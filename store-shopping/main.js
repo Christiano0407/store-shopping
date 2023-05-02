06c3ac4ba1b3431a4f0ebd6144bca9be381464a0
Christiano0407 <placebo0704@gmail.com>
2023-05-02 12:50:46 -0600

    new add project

--- store-shopping/main.js
+++ store-shopping/main.js
@@ -1,10 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+//import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
-  <div>
+  <main class="main__container" id="main">
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
@@ -12,13 +12,7 @@ document.querySelector('#app').innerHTML = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  </main>
+`;
 
-setupCounter(document.querySelector('#counter'))
+//setupCounter(document.querySelector('#counter'));
