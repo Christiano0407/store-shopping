c841d1d5667750a25f998057def2cb6798fcaddd
Christiano0407 <placebo0704@gmail.com>
2023-05-03 09:23:42 -0600

    new files and mobile first and css style system

--- store-shopping/main.js
+++ store-shopping/main.js
@@ -1,18 +1,8 @@
 import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
 //import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
-  <main class="main__container" id="main">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-  </main>
+ 
 `;
 
 //setupCounter(document.querySelector('#counter'));
